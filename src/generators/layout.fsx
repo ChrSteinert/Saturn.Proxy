9d269187e042785798d559b217d3f347180c9655
Christian Steinert <chr.steinert@outlook.com>
2020-05-02 21:11:14 +0200

    Adds Maven, removes Navbar.

--- src/generators/layout.fsx
+++ src/generators/layout.fsx
@@ -26,6 +26,7 @@ let injectWebsocketCode (webpage:string) =
       window.addEventListener("load", init, false);
       </script>
         """
+    let websocketScript = ""
     let head = "<head>"
     let index = webpage.IndexOf head
     webpage.Insert ( (index + head.Length + 1),websocketScript)
@@ -50,29 +51,15 @@ let layout (ctx : SiteContents) active bodyCnt =
             meta [CharSet "utf-8"]
             meta [Name "viewport"; Content "width=device-width, initial-scale=1"]
             title [] [!! ttl]
-            link [Rel "icon"; Type "image/png"; Sizes "32x32"; Href "/images/favicon.png"]
+            ``base`` [Href "https://chrsteinert.github.io/Saturn.Proxy/"]
+            link [Rel "icon"; Type "image/png"; Sizes "32x32"; Href "images/favicon.png"]
             link [Rel "stylesheet"; Href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"]
-            link [Rel "stylesheet"; Href "https://fonts.googleapis.com/css?family=Open+Sans"]
+            link [Rel "stylesheet"; Href "https://fonts.googleapis.com/css?family=Maven+Pro"]
             link [Rel "stylesheet"; Href "https://unpkg.com/bulma@0.8.0/css/bulma.min.css"]
-            link [Rel "stylesheet"; Type "text/css"; Href "/style/style.css"]
+            link [Rel "stylesheet"; Type "text/css"; Href "style/style.css"]
 
         ]
         body [] [
-          nav [Class "navbar"] [
-            div [Class "container"] [
-              div [Class "navbar-brand"] [
-                a [Class "navbar-item"; Href "/"] [
-                  img [Src "/images/bulma.png"; Alt "Logo"]
-                ]
-                span [Class "navbar-burger burger"; Custom ("data-target", "navbarMenu")] [
-                  span [] []
-                  span [] []
-                  span [] []
-                ]
-              ]
-              div [Id "navbarMenu"; Class "navbar-menu"] menuEntries
-            ]
-          ]
           yield! bodyCnt
         ]
     ]
